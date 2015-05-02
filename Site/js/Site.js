bb599e0b392c325ca72ad8f71abce6c0de31acad
Xesyto <s.oudenaarden@gmail.com>
2015-05-02 11:50:38 +0200

    Moved which root generator gets loaded into html instead of hardcoded in JS.

--- Site/js/Site.js
+++ Site/js/Site.js
@@ -1,13 +1,24 @@
 /* global $ */
+var rootGeneratorPath = "";
+var rootGeneratorName = "";
+
 $( document ).ready(function( $ ) {
   //Load up all generators that will be used
-  loadGenerator("generators\\table\\Names.json");
-  
-  
+  rootGeneratorPath = $("#GeneratorFile").val();
+  rootGeneratorName = $("#GeneratorName").val();  
+    
+  loadGenerator(rootGeneratorPath);
+    
   //Because the generators are loaded through ajax we wait until  
   //all ajax activity has stopped before starting to attempt generating
   $( document ).ajaxStop(function() {
-    var generatedContent = generateFromTable("Names");
-    $("#content").append(generatedContent);
+    generateContent();
   });
 });
+
+function generateContent()
+{
+    var generatedContent = generateFromTable(rootGeneratorName);
+    $("#content").empty();
+    $("#content").append(generatedContent);
+}
