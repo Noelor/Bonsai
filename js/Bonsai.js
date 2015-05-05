c451bfa401aaf3a13fd4f386c81c3e6659d3a783
Xesyto <s.oudenaarden@gmail.com>
2015-05-05 12:19:46 +0200

    Uploading Bonsai Page

---
+++ js/Bonsai.js
@@ -0,0 +1,287 @@
+/* global $ */
+var generatorsTable = {};
+var generatorsContent = {};
+
+//Load a table type generator
+function loadGenerator(filePath,callBack) {			
+	console.log(filePath)
+	$.getJSON( filePath, function(Generator) {				
+		if(Generator.Type)
+		{
+			if(Generator.Type == "Table")
+			{
+				if(Generator.Name in generatorsTable == false)
+				{
+					generatorsTable[Generator.Name] = Generator.Data;
+					console.log(Generator.Name + " loaded");
+				}
+			}else
+			{
+				if(Generator.Name in generatorsContent == false)
+				{
+					generatorsContent[Generator.Name] = Generator;
+					console.log(Generator.Name + " loaded");
+				}
+			}
+		}else
+		{
+			console.error("Missing type in " + filePath + " file, can't load.");
+		}
+					
+		//Load listed dependencies, if any	
+		if("Dependencies" in Generator)
+		{
+			var dependencies = Generator.Dependencies.split(";");
+			$.each(dependencies, function(_,dependency)
+			{
+				loadGenerator(dependency);
+			});
+		}
+		
+		if(callBack)
+		{
+			callBack(Generator.Name);
+		}
+	});
+}
+
+//Get the title and description of a Content Generator
+function getContentPageInformation(contentName)
+{
+	var Information = {};
+	
+	if(contentName in generatorsContent)
+	{
+		var contentGenerator = generatorsContent[contentName];
+					
+		if("Title" in contentGenerator)
+		{
+			Information["Title"] = contentGenerator["Title"];									
+		}
+		
+		if("Description" in contentGenerator)
+		{
+			Information["Description"] = contentGenerator["Description"];
+		}
+				
+	}else{
+		console.error("Content Generator " + contentName + " is not loaded.");
+	}
+	
+	return Information;
+}
+
+//Generate the text entries from a Content Generator
+function generateFromContent(contentName)
+{
+	if(contentName in generatorsContent)
+	{
+		var content = "";
+		
+		var contentGenerator = generatorsContent[contentName].Data;
+		$.each(contentGenerator, function(_,section){
+			
+			//set properties not specified in json to default
+			if("Chance" in section == false)
+				section.Chance = 100;
+			
+			if("MinAmount" in section == false)
+				section.MinAmount = 1;
+			
+			if("MaxAmount" in section == false)
+				section.MaxAmount = 1;
+			
+			if("ParagraphType" in section == false)
+				section.ParagraphType = "Paragraph";
+			
+			//Dispatch to specific paragraph type methods
+			if(section.ParagraphType == "Paragraph" || section.ParagraphType == "Paragraph-Start" 
+			|| section.ParagraphType == "Paragraph-End")
+			{
+				content += generateContentParagraph(section);
+			}else if (section.ParagraphType == "List" || section.ParagraphType == "List-Start" 
+			|| section.ParagraphType == "List-End") {
+				content += generateContentList(section);
+			}else{
+				//Fallback to no decorations on unspecified type.
+				content += generateContentUndecorated(section);
+			}
+		});
+				
+		return content;
+	}else{
+		console.error("Content Generator " + contentName + " is not loaded.");
+	}
+}
+
+//generate a Content Generator section with Paragraph decoration
+function generateContentParagraph(section)
+{
+	var content = "";
+	var chanceR = Math.floor((Math.random() * 100) +1);	
+	
+	if( chanceR < section.Chance)
+	{		
+		if(section.ParagraphType != "Paragraph-End")
+			content+="<p>";
+						
+		var amountR = Math.floor((Math.random() * section.MaxAmount) + section.MinAmount);						
+		for(var i = 0;  i<amountR; i++)
+		{			
+			content += processNestedEntries(section.Text);					
+		}
+		
+		if(section.ParagraphType != "Paragraph-Start")
+			content+="</p>";	
+	}
+	
+	return content;
+}
+
+//generate a Content Generator section with List decorations
+function generateContentList(section)
+{
+	var content = "";
+	var chanceR = Math.floor((Math.random() * 100) +1);	
+	
+	if( chanceR < section.Chance)
+	{		
+		if(section.ParagraphType != "List-End")
+			content+="<ul>";
+		
+		var amountR = Math.floor((Math.random() * section.MaxAmount) + section.MinAmount);						
+		for(var i = 0;  i<amountR; i++)
+		{			
+			content += "<li>" + processNestedEntries(section.Text) + "</li>";					
+		}
+		if(section.ParagraphType != "List-Start")			
+			content+="</ul>";
+	}
+	
+	return content;	
+}
+
+//generate a Content Generator section without any decorations. 
+function generateContentUndecorated(section)
+{
+	var content = "";
+	var chanceR = Math.floor((Math.random() * 100) +1);	
+	
+	if( chanceR < section.Chance)
+	{				
+		var amountR = Math.floor((Math.random() * section.MaxAmount) + section.MinAmount);						
+		for(var i = 0;  i<amountR; i++)
+		{			
+			content += processNestedEntries(section.Text);					
+		}		
+	}
+	
+	return content;		
+}
+
+//Generate the text entries from a Table
+function generateFromTable(tableName)
+{	
+		if(tableName in generatorsTable)
+		{
+			var content="";
+			
+			var table = generatorsTable[tableName];
+			$.each(table, function(_, section)
+			{
+				//Grab a random entry from the section
+				var totalWeight = totalWeightTableSection(section);				
+				var randomEntry = getRandomTableSectionEntryByWeight(section, totalWeight);
+				
+				//Find any nested references to generators and process those.
+				randomEntry = processNestedEntries(randomEntry);
+								
+				//Add to content																	
+				content += randomEntry;
+			});
+						
+			return content;
+		}else{
+			console.error("Table Generator " + tableName + " is not loaded.");
+		}
+		
+}
+
+//Find references to generators in a string and replace them with the generators content
+function processNestedEntries(entry)
+{
+	var regex = /\$(\w)-(\w+)\$/g;
+	var match;																		
+	//Modifying the original string during regex.exec resets the progress.
+	//So copy it and regex over the copy while modifying the original
+	var regexString = entry;  
+									
+	//Recurse for nested entries.
+	while ((match = regex.exec(regexString)) !== null) {
+		var insert = "";
+		if(match[1] == "T")
+		{						
+			insert = generateFromTable(match[2]);
+			entry = entry.replace(match[0],insert);										
+		}else if(match[1] == "C")
+		{
+			insert = generateFromContent(match[2]);
+			entry = entry.replace(match[0],insert);
+		}
+		else if(match[1] == "R")
+		{
+			var numbers = match[2].split("_");			
+			var r = Math.floor((Math.random() * numbers[1]) + numbers[0]);
+			entry = entry.replace(match[0],r);
+		}
+		
+	}	
+	return entry;
+}
+
+//Add up all the weight in a Sections
+function totalWeightTableSection(section)
+{
+	var weight = 0;
+	
+	//weight is not relevant if there is only one entry 
+	//one entry sections can exist to include a whitespace or fixed strings.
+	if(Object.keys(section).length == 1)
+	{
+		return 1;
+	}
+	
+	$.each(section, function(_, entry)
+	{		
+		$.each(entry, function(_,w){
+			weight+=w;
+		});
+	});	
+	
+	return weight;
+}
+
+//Return an entry from a Table Section randomly but modified by weight. 
+function getRandomTableSectionEntryByWeight(section, totalweight)
+{
+	var r = Math.floor((Math.random() * totalweight));	
+	var returnText = "";
+	var finished = false; //Can't neatly break out of a jquery .each so use a manual termination.
+	
+	$.each(section, function(_, entry)
+	{		
+		$.each(entry, function(text,weight){						
+			
+			if( r < weight && 1 && !finished)
+			{					
+				returnText = text;
+				finished = true;									
+			}else if(!finished)
+			{			
+				r-= weight;
+			}
+		});
+	});	 
+	
+	return returnText;
+}
---
+++ js/Site.js
@@ -0,0 +1,53 @@
+/* global $ */
+var rootGeneratorPath = "";
+var rootGeneratorName = "";
+
+$( document ).ready(function( $ ) {
+  //Grab generator information from ulr or fallback to default on defined on page
+  var generator = getUrlParameter("gen");  
+  if(generator)
+  {    
+    rootGeneratorPath = "generators/content/" + generator + ".json";
+  }else
+  {    
+    rootGeneratorPath = $("#DefaultFile").val();    
+  }
+        
+  //Load up root generator that will be used  
+  loadGenerator(rootGeneratorPath,function(generatorName)
+  {
+    rootGeneratorName = generatorName;
+  });
+    
+  //Because the generators are loaded through ajax we wait until  
+  //all ajax activity has stopped before starting to attempt generating
+  $( document ).ajaxStop(function() {
+    var info = getContentPageInformation(rootGeneratorName);
+           
+    $("#BonsaiTitle").html(info.Title);
+    $("#BonsaiDescription").html(info.Description);
+        
+    generateContent();
+  });
+});
+
+function generateContent()
+{
+    var generatedContent = generateFromContent(rootGeneratorName);
+    $("#BonsaiContent").html(generatedContent);
+}
+
+//Source: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html through http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
