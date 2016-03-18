c27b0bd7bbd2685643fa0d6cfe3d1a760ac6eb1f
Xesyto <s.oudenaarden@gmail.com>
2016-03-18 19:11:31 +0100

    Added c# like string.format prototype

--- Site/js/Bonsai.js
+++ Site/js/Bonsai.js
@@ -451,3 +451,13 @@ function getRandomNumberInclusive(min, max) {
     var r   =  Math.floor(Math.random() * (max - min + 1)) + min;       
     return r;
 }
+
+String.prototype.format = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
