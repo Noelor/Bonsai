383835542608ba50efa312b5336b0477382540ad
Xesyto <s.oudenaarden@gmail.com>
2016-04-08 20:12:06 +0200

    Site: Updated Bonsai.js

--- js/Bonsai.js
+++ js/Bonsai.js
@@ -175,7 +175,17 @@ function generateFromContent(contentName, clearData)
             }else{
 				//Fallback to no decorations on unspecified type.
 				content += generateContentUndecorated(section,poolName);
-			}								
+			}
+                             
+            if("SentenceCase" in section == true)
+            {               
+ 
+                if(section.SentenceCase == "Title-Case")
+                {
+ 
+                    content = content.toTitleCase();
+                }
+            }								
 		});
 				
 		return content;
@@ -494,3 +504,30 @@ String.prototype.format = function() {
     }
     return s;
 };
+
+
+//Source: http://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+String.prototype.toTitleCase = function() {
+  var i, j, str, lowers, uppers;
+  str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+
+  // Certain minor words should be left lowercase unless 
+  // they are the first or last words in the string
+  lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
+  'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+  for (i = 0, j = lowers.length; i < j; i++)
+    str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+      function(txt) {
+        return txt.toLowerCase();
+      });
+
+  // Certain words such as initialisms or acronyms should be left uppercase
+  uppers = ['Id', 'Tv'];
+  for (i = 0, j = uppers.length; i < j; i++)
+    str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+      uppers[i].toUpperCase());
+
+  return str;
+}
